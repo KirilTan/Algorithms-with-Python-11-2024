9da78738223889de8d5c7e2d6f0cc13fc6e5018c
kiril <kiriltanev@proton.me>
2024-11-14 21:36:15 +0200

    feat: lab/01 solution added

---
+++ 01_recursion/lab/01_recursive_array_sum.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def array_sum(nums: List[int], idx: int = 0) -> int:
+    """
+    Recursively calculates the sum of a list of integers.
+
+    Args:
+        nums (List[int]): A list of integers to be summed.
+        idx (int): The current index in the list to be processed. Defaults to 0.
+
+    Returns:
+        int: The sum of the integers in the list from the current index to the end.
+    """
+    if idx == len(nums) - 1:
+        return nums[idx]
+    return nums[idx] + array_sum(nums, idx + 1)
+
+# # Example:
+# input_nums = [int(x) for x in input().split()]
+#
+# print(array_sum(input_nums))
