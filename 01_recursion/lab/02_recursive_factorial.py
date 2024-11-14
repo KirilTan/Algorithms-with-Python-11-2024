fdeb8c1ecdbcdbd6423307367126dc1ccfcd3e89
kiril <kiriltanev@proton.me>
2024-11-14 22:00:46 +0200

    chore: docstring added

--- 01_recursion/lab/02_recursive_factorial.py
+++ 01_recursion/lab/02_recursive_factorial.py
@@ -1,4 +1,13 @@
 def factorial(n: int) -> int:
+    """
+    Calculate the factorial of a given number using recursion.
+
+    Parameters:
+        n (int): A positive integer for which the factorial is to be calculated.
+
+    Returns:
+        int: The factorial of the given number.
+    """
     if n == 1:
         return 1
     return n * factorial(n-1)
