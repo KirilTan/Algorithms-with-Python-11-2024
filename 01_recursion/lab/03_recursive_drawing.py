5022f6adfc98f5f2de14c39c4cdc7a6f324ff672
kiril <kiriltanev@proton.me>
2024-11-15 16:31:22 +0200

    chore: docstring added

--- 01_recursion/lab/03_recursive_drawing.py
+++ 01_recursion/lab/03_recursive_drawing.py
@@ -1,4 +1,18 @@
 def recursive_drawing(n: int, first_part: str = '*', second_part: str = '#') -> None:
+    """
+    Draws a recursive pattern using two different characters.
+
+    This function prints a pattern of lines with decreasing and then increasing
+    lengths, using two specified characters. The pattern is drawn recursively.
+
+    Parameters:
+        n (int): The number of lines to draw in the first part of the pattern.
+        first_part (str): The character used to draw the first part of the pattern. Defaults to '*'.
+        second_part (str): The character used to draw the second part of the pattern. Defaults to '#'.
+
+    Returns:
+        None: This function does not return a value.
+    """
     if n == 0:
         return None
 
